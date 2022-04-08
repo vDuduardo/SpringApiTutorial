bc8d8c61cc58255a1d578110e7415606eb0c2cdb
vDuduardo <everbinen@gmail.com>
2022-04-08 16:26:21 -0300

    Build of the Project and First Commit

---
+++ src/main/java/com/bts/imageclient/SpringApiTutorial/SpringApiTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.bts.imageclient.SpringApiTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bts/imageclient/SpringApiTutorial/SpringApiTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bts.imageclient.SpringApiTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
